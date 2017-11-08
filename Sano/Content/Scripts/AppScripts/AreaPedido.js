8c8cbdc2bf021f6fb41481466f24274a05349f76
LuisRicardoV <l.ricardovc@gmail.com>
2017-11-08 17:47:07 -0600

    ACTUALIZACION 20171108

--- Sano/Content/Scripts/AppScripts/AreaPedido.js
+++ Sano/Content/Scripts/AppScripts/AreaPedido.js
@@ -5,16 +5,15 @@ window.AreaPedido = {
     Init: function (parameters) {
         $.extend(AreaPedidoData, parameters);
         $('body').on('click', '[data-toggle]', this.ActionOpenSideBar);
-        $('body').on('click', '.item-menu', this.ActionMenu);
+        $('body').on('click', '.item-menu', this.ActionMenuContainerDETALLE);
         $('body').on('click', '.Platillo', this.ActionPadNumerico);
-        $('body').on('click', '#BtnBackMenu', this.ActionBackMenu);
+        $('body').on('click', '#btnBackMenuGENERAL', this.ActionBackMenuGENERAL);
+        $('body').on('click', '#btnBackMenuDETALLE', this.ActionBackMenuDETALLE);
+        $('body').on('click', '#btnBackMenuPADNUMERICO', this.ActionBackMenuPADNUMERICO);
         $('body').on('click', '.val-PADNUMERICO', this.ActionPadNumericoCalc);
         $('body').on('click', '.val-PADNUMERICO-ENTER', this.ActionPadNumericoEnter);
         $('body').on('click', '.val-PADNUMERICO-DELETE', this.ActionPadNumericoDel);
         $('body').on('click', '#BtnAdd', this.ActionBtnAdd);
-        $('body').on('click', '#BtnBack', this.ActionBtnBack);
-
-
 
         $(".swipe-area").on("swipe", function (event, phase, direction, distance, duration, fingers) {
             event.preventDefault();
@@ -33,20 +32,25 @@ window.AreaPedido = {
         var toggle_el = $(this).data("toggle");
         $(toggle_el).toggleClass("open-sidebar");
     },
-    ActionMenu: function (event) {
+    ActionMenuContainerDETALLE: function (event) {
         event.preventDefault();
-        $("#menu-container").hide(500);
-        $("#menu-container-PLATILLO").show(500);
+        $("#menu-container-GENERAL").hide(300);
+        $("#menu-container-DETALLE").show(300);
     },
-    ActionBackMenu: function (event) {
+    ActionBackMenuGENERAL: function (event) {
         event.preventDefault();
-        $("#menu-container").show(500);
-        $("#menu-container-PLATILLO").hide(500);
+        $("#menu-container-DETALLE").hide(300);
+        $("#menu-container-GENERAL").show(300);
+    },
+    ActionBackMenuDETALLE: function (event) {
+        event.preventDefault();
+        $("#menu-container-PADNUMERICO").hide(300);
+        $("#menu-container-DETALLE").show(300);
     },
     ActionPadNumerico: function (event) {
         event.preventDefault();
-        $("#menu-container-PLATILLO").hide(500);
-        $("#menu-container-PADNUMERICO").show(500);
+        $("#menu-container-DETALLE").hide(300);
+        $("#menu-container-PADNUMERICO").show(300);
     },
     ActionPadNumericoCalc: function (event) {
         event.preventDefault();
@@ -56,8 +60,8 @@ window.AreaPedido = {
     },
     ActionPadNumericoEnter: function (event) {
         event.preventDefault();
-        $("#menu-container-PADNUMERICO").hide();
-        $("#menu-container-COMPLEMENTOS").show();
+        $("#menu-container-PADNUMERICO").hide(300);
+        $("#menu-container-COMPLEMENTOS").show(300);
     },
     ActionPadNumericoDel: function (event) {
         event.preventDefault();
@@ -66,13 +70,12 @@ window.AreaPedido = {
     },
     ActionBtnAdd: function (event) {
         event.preventDefault();
-       
     },
 
-    ActionBtnBack: function (event) {
+    ActionBackMenuPADNUMERICO: function (event) {
         event.preventDefault();
-        $("#menu-container-PADNUMERICO").show(500);
-        $("#menu-container-COMPLEMENTOS").hide(500);
+        $("#menu-container-COMPLEMENTOS").hide(300);
+        $("#menu-container-PADNUMERICO").show(300);
     },
   
 };
